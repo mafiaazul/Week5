7bbd6f08e78f05642ec2a12016ef45fa9c7daceb
rodrigogrocha@hotmail.com <Cruzeiro.2015>
2017-10-09 10:14:47 -0500

    latest

---
+++ src/CompareNames.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CompareNames {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Please enter 1st name: ");
+		String firstName = in.nextLine();
+		
+		System.out.print("Please enter 2nd name: ");
+		String secondName = in.nextLine();
+		
+		if (firstName.equalsIgnoreCase(secondName))
+			System.out.print("Both names are equal");
+		else 
+			System.out.print("Names are different!");
+		
+		in.close();
+
+	}
+
+}
